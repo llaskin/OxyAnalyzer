7d9d43d02370558a60c64d5b4e9ec774cf72d197
Leo Laskin <llaskin@gmail.com>
2017-01-04 19:45:27 -0600

    commenting out handling anything until we hook up the ADC

--- analyzer.ino
+++ analyzer.ino
@@ -10,9 +10,10 @@ const int buzzerPin = 9; //buzzer to arduino pin 9
 
 //General Calibration setup
 double calbrationPre, calibrationPost, calibrationPressure;
-int Sensor_lowrange=58;//When sensor is healthy and new it reads 58 on low
-int Sensor_highrange=106;//When sensor is healthy and new it reads 106 on high
-int current_function=0;
+int Sensor_lowrange = 58;//When sensor is healthy and new it reads 58 on low
+int Sensor_highrange = 106;//When sensor is healthy and new it reads 106 on high
+int current_function = 0;
+int pressureZero = 0;
 
 double resultPre;//After calculations holds the current O2 percentage
 double resultPost;//After calculations holds the current O2 percentage
@@ -20,10 +21,9 @@ double resultPressure;//After calculations holds the current O2 percentage
 
 
 void setup(void)
-{
-  Serial.begin(9600);
+{  
   pinMode(buzzerPin, OUTPUT); // Set buzzer - pin 9 as an output
-
+  Serial.begin(9600);
     //LCD Setup
   display.begin(20, 4);  //20 character, 4 line display
   display.setCursor(0, 0);
@@ -31,19 +31,18 @@ void setup(void)
   Serial.println("Analyzer V1");
 
   //starts ADS readings
-  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+//  ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
   ads.begin();
-
   display.setCursor(0, 1);
+  Serial.println("Calibrating");
   display.print("Calibrating");
-  Serial.println("Calibrating");  
   checkCalibration(calibratePreCompressorSensor(),   calibratePostCompressorSensor(), zeroPressureSensor());
-
+  Serial.println("calibrated");
 }
 //Checks calibration
 void checkCalibration(int calPre, int calPost, int calPress)
 {
-  delay(10000);  //delay since JJ sensors take 6 seconds to calibrate
+  delay(6000);  //delay since JJ sensors take 6 seconds to calibrate
 //  if ((calPre > Sensor_highrange) || (calPre < Sensor_lowrange))
 //  {
 //    current_function=1;//Sensor needs to be calibrated
@@ -54,11 +53,15 @@ void checkCalibration(int calPre, int calPost, int calPress)
 //    current_function=1;//Sensor needs to be calibrated
 //    need_calibrating("Post", calPost);//print need calibrating message
 //   }
-//  if(calPress > 100)
-//  {
-//    current_function = 1;
-//    need_calibrating("Pressure", calPress);
-//  }
+  if((calPress < 90) || (calPress > 140))
+  {
+    current_function = 1;
+    need_calibrating("Pressure", calPress);
+  }
+  else
+  {
+    pressureZero = calPress; 
+  }
 }
 //Prints need calibrating text
 void need_calibrating(String sensor, double calibrationValue)
@@ -66,16 +69,12 @@ void need_calibrating(String sensor, double calibrationValue)
   display.clear();
   display.setCursor(0,0);
   display.print("Sensor error");
-  Serial.println("Sensor error");
   display.setCursor(0, 1);
   display.print("Please");
-  Serial.println("Sensor error");
   display.setCursor(0, 2);
   display.print("calibrate " + sensor);
-  Serial.println("calibrate " + sensor);
   display.setCursor(0, 3);
   display.print(calibrationValue);
-  Serial.println(calibrationValue);
   current_function = 2;
 
 }
@@ -100,85 +99,94 @@ int calibrateSensor(int sensorID)
 //Calibrate the sensor that is pre-compression
 int calibratePreCompressorSensor()
 {
-  return calibrateSensor(0);
+  //return calibrateSensor(0);
+  return 0;
 }
 //Calibrate sensor which is on the exhaust side of the compressor
 int calibratePostCompressorSensor()
 {
-  return calibrateSensor(1);
+  //return calibrateSensor(1);
+  return 0;
 }
 //Calibrate the Pressure sensor with known gas source(Zero pressure in tanks when initial boot)
 int zeroPressureSensor()
 {
-  return calibrateSensor(2);
+  return analogRead(A5);
 }
 
 int analyzePreSensor()
 {
-  return readADC(0);
+  return 0;
+  //return readADC(0);
 }
 
 int analyzePostSensor()
 {
-  return readADC(1);
+  return 0;
+//  return readADC(1);
 }
 
 int analyzePressure()
 {
-  return readADC(2);
+  int pressureRMV = analogRead(A5);
+  double PSI = (map(pressureRMV,pressureZero,1023,0,4500));
+  return PSI;
 }
 int printAnalysisData()
 {
   display.clear();
-  delay(3000);
+  delay(1000);
   display.setCursor(0, 0);
   display.print("O2% and Pressure");
   display.setCursor(0, 1);
-  display.print("Pre: " + String(resultPre));
+//  display.print("Pre: " + String(resultPre));
   display.setCursor(0, 2);
-  display.print("Post: " + String(resultPost));
+//  display.print("Post: " + String(resultPost));
   display.setCursor(0, 3);
   display.print("Pressure: " + String(resultPressure));
   Serial.println("O2% and Pressure");
-  Serial.println("Pre: " + String(resultPre));
-  Serial.println("Post: " + String(resultPost));
+//  Serial.println("Pre: " + String(resultPre));
+//  Serial.println("Post: " + String(resultPost));
   Serial.println("Pressure: " + String(resultPressure));
 }
 void loop(void)
 {
   //********  Main Loop variable declaration ***********
-  double currentmvPre = 0.0; //the current mv put out by the oxygen sensor;
-  double currentmvPost = 0.0; //the current mv put out by the oxygen sensor;
-  double currentPressure = 0.0; //the current mv put out by the oxygen sensor;
+  double currentmvPre; //the current mv put out by the oxygen sensor;
+  double currentmvPost; //the current mv put out by the oxygen sensor;
+  double currentPressure; //the current mv put out by the oxygen sensor;
 
   switch(current_function)
   {
     case 0: //Analyze Gas
-      currentmvPre = analyzePreSensor();
+      Serial.println("in case 0");
+      Serial.println("resultPre");
+      currentmvPre = analyzePreSensor();      
       resultPre = (currentmvPre/calbrationPre)*20.9;
-      currentmvPost = analyzePostSensor();
+      Serial.println("resultPost");
+      currentmvPre = analyzePostSensor();
       resultPost = (currentmvPost/calibrationPost)*20.9;
+      Serial.println("about to read Pressure");
       resultPressure = analyzePressure();
-      Serial.println("========");
-      Serial.println(currentmvPre);
-      Serial.println(calbrationPre);
-      Serial.println(currentmvPost);
-      Serial.println(calibrationPost);
-      Serial.println(resultPressure);
       //Write to display
+      Serial.println("about to print");
       printAnalysisData();
+      delay(5000); //retest pressure and o2% every 5 seconds
       break;
 
     case 1:  //Calibrate sensor
+    Serial.println("in case 1");
       display.clear();
-      display.setCursor(0, 0);
+      display.setCursor(0, 1);
       display.print("Calibrating");
+      Serial.println("in case 1, Calibrating");
 
       current_function=0;//O2 analyzing
       checkCalibration(calibratePreCompressorSensor(),   calibratePostCompressorSensor(), zeroPressureSensor());
       break;
 
     case 2:  //Error buzzer
+    Serial.println("in case 2");
       tone(buzzerPin, 1000); // Send 1KHz sound signal...
       delay(1000);        // ...for 1 sec
       noTone(buzzerPin);     // Stop sound...
